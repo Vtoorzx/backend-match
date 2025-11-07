d734a4dda00213be75f4e12d00a1436b7bf652db
victor araujo <email.victornascimento@gmail.com>
2025-11-07 00:01:04 -0300

    Configuração do cors no backend

--- src/app.js
+++ src/app.js
@@ -3,17 +3,41 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
-
-
+const cors = require('cors'); 
 
 connectDB();
 
 const app = express();
+
+
+const ALLOWED_ORIGIN = 'https://match-c35f.vercel.app'; 
+
+const corsOptions = {
+   
+    origin: (origin, callback) => {
+        if (origin === ALLOWED_ORIGIN || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS Policy: ' + origin));
+        }
+    },
+    credentials: true,
+    optionsSuccessStatus: 200
+};
+
+
+app.use(cors(corsOptions));
+
+
+app.use(express.json());
+
 app.get('/', (req, res)=> {
     res.send('Match+ rodando!');
 });
 
+
 app.use('/api/auth', authRoutes);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log('Servidor rodando na porta ${PORT}'));
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
