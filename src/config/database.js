585730be6462f3849725fd0b404cc4ed09d7b12b
victor araujo <email.victornascimento@gmail.com>
2025-11-12 09:39:13 -0300

    atualização do banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -11,7 +11,7 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      serverSelectionTimeoutMS: 5000, 
+      serverSelectionTimeoutMS: 5000 
     });
 
     console.log('✅ MongoDB conectado com sucesso!');
